d7d8447eaee15ecd8bb8dcbd33fa905ee00644dd
nocmt <nocmtone@gmail.com>
2019-02-17 11:45:30 +0800

    修复一些问题

--- html/js/index.js
+++ html/js/index.js
@@ -20,53 +20,81 @@ function getTime() {
         (sec < 10 ? ("0" + sec) : sec);
 }
 
-
+let htmls = {};
 // 选项卡切换
-function switchTab(dataId) {
-    // 请求缓存，避免重复请求
-    // 待开发
-
+function switchTab() {
+    let html = '';
+    let item = '';
+    let dataId = this.getAttribute('data-id');;
+    // console.log(dataId);
     // 选项切换标识
     let ats = document.getElementsByTagName("li");
-    for(let v of ats) {
-        if((v.getAttribute('data-id') === dataId)){
+    for (let v of ats) {
+        if ((v.getAttribute('data-id') === dataId)) {
             v.classList.add('menu-action');
-        }else{
+        } else {
             v.classList.remove('menu-action');
         }
-      }
+    }
 
-    let html = "";
-    let item = "";
-    ajax({
-        url: 'http://nocmt.com/test/bookmark.json', // 请求地址
-        type: 'POST', // 请求类型，默认"GET"，还可以是"POST"
-        data: {
-            // 'sort': sort
-        }, // 传输数据
-        dataType: "json",
-        success: function (response, xml) { // 请求成功的回调函数
-            // console.log(JSON.parse(response));
-            let result = JSON.parse(response);
-            if (result.success) {
-                // console.log(result.success);
-                item = result.result.data;
-                for (let i = 0; i < item.length; i++) {
-                    // console.log(item[i].id);
-                    html += `<a target="_blank" href="//${item[i].link}">
-                    <img width="20" src="${item[i].imgurl}" alt="">
-                    <span class="text">${item[i].title}</span>
-                </a>`;
-                    // console.log(html);
+    // 请求缓存，避免重复请求
+    if (htmls[dataId]) {
+        document.getElementById('main').innerHTML = htmls[dataId];
+    } else {
+        ajax({
+            url: 'http://nocmt.com/test/bookmark.json', // 请求地址
+            type: 'POST', // 请求类型，默认"GET"，还可以是"POST"
+            data: {
+                // 'sort': sort
+            }, // 传输数据
+            dataType: "json",
+            success: function (response, xml) { // 请求成功的回调函数
+                // console.log(JSON.parse(response));
+                let result = JSON.parse(response);
+                if (result.success) {
+                    // console.log(result.success);
+                    item = result.result.data;
+                    for (let i = 0; i < item.length; i++) {
+                        // console.log(item[i].id);
+                        html += `<a target="_blank" href="//${item[i].link}">
+                        <img width="20" src="${item[i].imgurl}" alt="">
+                        <span class="text">${item[i].title}</span>
+                    </a>`;
+                        // console.log(html);
+                    }
+                    htmls[dataId] = html;
+                    document.getElementById('main').innerHTML = html;
                 }
-                document.getElementById('main').innerHTML = html;
-            }
 
-        },
-        fail: function (status) {
-            console.log(status);
-        } // 请求失败的回调函数
-    });
+            },
+            fail: function (status) {
+                console.log(status);
+            } // 请求失败的回调函数
+        });
+        // console.log(htmls);
+    }
+
+}
+// 延迟hover事件
+// let tio;
+
+// function addTimeout() {
+//     let dataId = this.getAttribute('data-id');
+//     tio = setTimeout(function () {
+//         switchTab(dataId);
+//     }, 1500);
+// }
+
+// function closeTimeout() {
+//     clearTimeout(tio);
+// }
+
+
+let ats = document.getElementsByTagName("li");
+for (let v of ats) {
+    v.onclick = switchTab;
+    // v.onmouseover = addTimeout;
+    // v.onmouseout = closeTimeout;
 }
 
 window.onload = () => {
