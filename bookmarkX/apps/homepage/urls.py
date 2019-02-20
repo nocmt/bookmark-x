5b89527786c1f69b98a64e0e2a6e282b20a7cbdf
nocmt <nocmtone@gmail.com>
2019-02-20 17:22:14 +0800

    简单测试

--- bookmarkX/apps/homepage/urls.py
+++ bookmarkX/apps/homepage/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('test/', views.TestView.as_view(), name='test')
 ]
--- bookmarkX/apps/homepage/views.py
+++ bookmarkX/apps/homepage/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import generics
 
+from .models import Users
+from .serializer import UsersSerializer
 
-def index(request):
-    pass
+# 使用APIView
+class TestView(APIView):
+    def get(self, request, format=None):
+        user = Users.objects.all()
+        serializer = UsersSerializer(user, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = UsersSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
